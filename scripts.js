be2307feb6ef7cbfca2e31b985766487f9501554
Rani-s123 <rajpurohitrani004@gmail.com>
2023-06-26 13:10:53 +0530

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,22 @@
+function myfunction(){
+    var x=document.getElementById("menu");
+    if(x.style.display=="block"){
+        x.style.display="none";
+    }else{
+        x.style.display="block";
+    }
+   }
+
+   var modal = document.getElementById('id01');
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
+}
+
+function Function(){
+    alert("Contact Number = 9986438391\n\nEmail Id = Smartclass004@gmail.com" );
+  }
+    
